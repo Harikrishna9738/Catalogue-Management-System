235f8003e6e9ac665c2a91d6e140f93d0b9afa14
harikrishna9738 <harikrishnayadav70@gmail.com>
2019-12-23 13:07:44 +0530

    created all the modules

--- Backend/CatalogueManagement/CatalogueApp/admin.py
+++ Backend/CatalogueManagement/CatalogueApp/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from .models import Brand,Product,Category,Specifications
 # Register your models here.
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('guest_name','govt_id','number_of_guest','age','contact','email')
+
+
+admin.site.register(Guest,GuestAdmin)
--- Backend/CatalogueManagement/CatalogueApp/models.py
+++ Backend/CatalogueManagement/CatalogueApp/models.py
@@ -22,3 +22,38 @@ class Category(models.Model):
             parent_category = parent_category.parent_category
         get_breadcrumbs.append(category.name)
         return get_breadcrumbs
+
+
+class Brand(models.Model):
+    """brand models,this has a tree structure using foriegn key.
+    the name is unique"""
+    name = models.CharField(max_length=250, unique=True, help_text="brand name,name must be unique")
+    date_created = models.DateTimeField(auto_now_add=True, help_text="this was when brand created")
+    last_modified = models.DateTimeField(auto_now_add=True, help_text="this was created when modified last")
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    """holds the brand and category is unique.a product can have multiple specifications"""
+    name = models.CharField(max_length=250, unique=True, help_text="product name,name must be unique")
+    brand = models.ForeignKey(Brand, help_text="the brand belongs to product", on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, help_text="the category belongs to product", on_delete=models.CASCADE)
+    date_created = models.DateTimeField(auto_now_add=True, help_text="this was when brand created")
+    last_modified = models.DateTimeField(auto_now_add=True, help_text="this was created when modified last")
+
+    def __str__(self):
+        return self.name
+
+
+class Specifications(models.Model):
+    """specifications models,about the product specifications"""
+    key = models.CharField(max_length=250, unique=True, help_text="the key of specification")
+    vale = models.CharField(max_length=250, unique=True, help_text="the value of specification")
+    unit = models.CharField(max_length=250, null=True, unique=True, help_text="the unit of specification")
+    date_created = models.DateTimeField(auto_now_add=True, help_text="this was when brand created")
+    last_modified = models.DateTimeField(auto_now_add=True, help_text="this was created when modified last")
+
+    def __str__(self):
+        return self.key + ' ,' + self.value + (' ' + self.unit if + self.unit else ' ')
